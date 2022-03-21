53fc4a948db56aab85f562cd68266192d4477f96
julian <j.feinauer@pragmaticminds.de>
2022-03-21 17:47:46 +0100

    Added Example for Secured Group Messages

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from olm import Account, OutboundSession, InboundSession
+from olm import Account, OutboundSession, InboundSession, OutboundGroupSession, InboundGroupSession, \
+    OlmGroupSessionError
 
 if __name__ == '__main__':
     alice = Account()
@@ -18,4 +19,30 @@ if __name__ == '__main__':
 
     response = bob_session.encrypt("Hey Alice")
     print(alice_session.decrypt(response))
+
+    # Try to do a MEGOLM session
+    sender = OutboundGroupSession()
+    secret_session_key = sender.session_key
+
+    group_messages = []
+    for i in range(1,100):
+        group_messages.append(sender.encrypt(f"Hallo da draußen {i}/99"))
+    print(group_messages)
+
+    # One receiver
+    receiver1 = InboundGroupSession(secret_session_key)
+
+    for msg in group_messages:
+        print(receiver1.decrypt(msg))
+
+    # Invite someone else
+    key_50 = receiver1.export_session(50)
+
+    receiver2 = InboundGroupSession.import_session(key_50)
+
+    for msg in group_messages:
+        try:
+            print(receiver2.decrypt(msg))
+        except OlmGroupSessionError:
+            print(" - secured - ")
 
